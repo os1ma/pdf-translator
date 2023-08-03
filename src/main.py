a82083cc7a508f68c0d02d297cc6cbc3b3e3782e
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-08-04 02:57:49 +0900

    Use streamlit

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import logging
 
-import click
+import streamlit as st
 from dotenv import load_dotenv
 from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import PyPDFLoader
 from langchain.schema import HumanMessage, SystemMessage
+from pypdf import PdfReader
 
 load_dotenv()
 
@@ -26,6 +26,7 @@ def translate(text: str) -> str:
     messages = [
         SystemMessage(
             content="Please translate the following English text into Japanese."
+            "Please output in markdown format."
         ),
         HumanMessage(content=text),
     ]
@@ -33,24 +34,49 @@ def translate(text: str) -> str:
     return chat(messages).content
 
 
-@click.command()
-@click.option("--file", "-f", help="Path to the PDF file")
-def main(file: str) -> None:
-    logger.info(f"Loading file {file}")
+st.set_page_config(layout="wide")
 
-    loader = PyPDFLoader(file_path=file)
-    pages = loader.load_and_split()
-    total_page_number = len(pages)
-    logger.info(f"Loaded {total_page_number} pages")
+st.title("pdf-translator")
+
+with st.sidebar:
+    uploaded_file = st.file_uploader("Upload a file", type=["pdf"])
+
+    clicked = st.button("Translate")
+
+if "translated_list" not in st.session_state:
+    st.session_state.translated_list = []
+
+if uploaded_file is not None:
+    file_name = uploaded_file.name
+    st.subheader(file_name)
+
+    pdf_reader = PdfReader(uploaded_file)
+    pages = pdf_reader.pages
+
+    translated_list = st.session_state.translated_list
+    next_translate_index = len(translated_list)
 
     for i, page in enumerate(pages):
-        logger.info(f"Translate page {i + 1}/{total_page_number}")
+        st.caption(f"{i + 1}/{len(pages)}")
 
-        text = page.page_content
-        translate(text)
+        left, right = st.columns(2)
 
-        input("Press Enter to continue...")
+        with left:
+            text = pages[0].extract_text()
+            st.text(text)
 
+        with right:
+            if i < len(translated_list):
+                st.text(translated_list[i])
+            elif i == next_translate_index:
+                if clicked:
+                    text_area = st.empty()
 
-if __name__ == "__main__":
-    main()
+                    translated_text = translate(text)
+
+                    text_area.markdown(translated_text)
+                    translated_list.append(translated_text)
+            else:
+                break
+
+        st.divider()
